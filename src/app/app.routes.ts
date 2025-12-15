2f7da7fda811b399750ee3652f4fcb4a78d4b2fc
Nis5680 <nishitshah5680@gmail.com>
2025-12-15 15:26:59 +0530

    2nd_change

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { About } from './about/about';
+import { Login } from './login/login';
+import { Contact } from './contact/contact';
+import { PageNotFound } from './page-not-found/page-not-found';
+import { User } from './user/user';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '' , component : Home},
+    {path: 'about' , component : About },
+    {path: 'login' , component : Login},
+    {path: 'contact', component : Contact},
+    {path: 'user/:id/:name', component : User},
+    {path: '**' , component : PageNotFound}
+];
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
-
+  users = [
+    {
+      id:'1',
+      name:'John Doe',
+      age:30,
+      email:'john@example.com'
+    },
+    {
+      id:'2',
+      name:'Jane Smith',
+      age:25,
+      email:'jane@example.com'
+    },
+    {
+      id:'3',
+      name:'Mike Johnson',
+      age:35,
+      email:'mike@example.com'
+    },
+    {
+      id:'4',
+      name:'Emily Davis',
+      age:28,
+      email:'emily@example.com'
+    },
+    {
+      id:'5',
+      name:'David Wilson',
+      age:32,
+      email:'david@example.com'
+    }
+  ];
 }
